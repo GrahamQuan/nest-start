be307c5a1399d717a449d0f1110f071652afe6df
GrahamQuan <grahamquan24@gmail.com>
2023-12-18 21:09:45 +0800

    middleware

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,9 +1,19 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
+import { LogMiddleware } from './middleware/log/log.middleware';
+import { AuthMiddleware } from './middleware/auth/auth.middleware';
 
 @Module({
   controllers: [UserController],
   providers: [UserService],
 })
-export class UserModule {}
+export class UserModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(LogMiddleware)
+      .forRoutes('user')
+      .apply(AuthMiddleware)
+      .forRoutes('user');
+  }
+}
