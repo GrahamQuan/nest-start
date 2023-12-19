e255843887e9e1172eea83f9d2572a156a1a67a6
GrahamQuan <grahamquan24@gmail.com>
2023-12-19 20:48:35 +0800

    setup prisma and swagger

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,16 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { PeopleModule } from './people/people.module';
+import { ConfigModule } from '@nestjs/config';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [UserModule, PeopleModule],
+  imports: [
+    UserModule,
+    PeopleModule,
+    ConfigModule.forRoot({ envFilePath: '.env' }),
+    PrismaModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
