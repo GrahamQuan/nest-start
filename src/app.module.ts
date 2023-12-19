3526b8aefe5c1a09edb99cc328ca0bc70a600eef
GrahamQuan <grahamquan24@gmail.com>
2023-12-19 21:36:34 +0800

    CRUD: prisma + cloud_db + swagger

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { UserModule } from './user/user.module';
 import { PeopleModule } from './people/people.module';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from './prisma/prisma.module';
+import { ArticlesModule } from './articles/articles.module';
 
 @Module({
   imports: [
@@ -12,6 +13,7 @@ import { PrismaModule } from './prisma/prisma.module';
     PeopleModule,
     ConfigModule.forRoot({ envFilePath: '.env' }),
     PrismaModule,
+    ArticlesModule,
   ],
   controllers: [AppController],
   providers: [AppService],
