e1081e9496af448a10d7fbcd2a406308831af6a0
GrahamQuan <grahamquan24@gmail.com>
2023-12-19 23:43:14 +0800

    clear old data

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UserModule } from './user/user.module';
-import { PeopleModule } from './people/people.module';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from './prisma/prisma.module';
 import { ArticlesModule } from './articles/articles.module';
 
 @Module({
   imports: [
-    UserModule,
-    PeopleModule,
     ConfigModule.forRoot({ envFilePath: '.env' }),
     PrismaModule,
     ArticlesModule,
